fe5d9b6d79a8eda376a7e24641ab2a8270db0d8d
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-10-17 17:05:42 +0200

    goodbye MKR1

--- MKR1/Client.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MKR1;
-
-public class Client
-{
-    public Guid Id { get; } = Guid.NewGuid();
-}
--- MKR1/ClientFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lab3.Network.Factories;
-
-namespace MKR1;
-
-public class ClientFactory : IJobFactory<Client>
-{
-    public Client Create() => new();
-}
